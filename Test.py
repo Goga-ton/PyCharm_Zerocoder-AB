0ed12c4fb90374e43e1c642dd31e5606cd6062b9
Goga-ton <goga_ton@mail.ru>
2025-09-25 10:02:25 +0300

    add html

--- Test.py
+++ Test.py
@@ -1,31 +1,13 @@
-import telebot
-import datetime
-import time
-import threading
-import random
-bot = telebot.TeleBot('7763593032:AAH9nUfnF94Cop22CPlBir1-2DIxMdAQGOg')
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.reply_to(message, "Привет! Это тестовое сообщение от чатБота. Для получения информации вводи запрос '/fact'")
-    reminder_thread = threading.Thread(target=send_reminder, args=(message.chat.id,))
-    reminder_thread.start()
-@bot.message_handler(commands=['fact'])
-def fact_message(message):
-    list = ['**Гидра — потенциально бессмертное существо**',
-            '**Крионика — путь к технологическому бессмертию**',
-            '**Информационное бессмертие через цифровизацию сознания**']
-    random_fact = random.choice(list)
-    bot.reply_to(message, f"Привет! Лови факт о бессмертии {random_fact}")
-def send_reminder(chat_id):
-    first_rem = "09:00"
-    second_rem = "14:00"
-    end_rem = "18:00"
-    while True:
-        now = datetime.datetime.now().strftime('%H:%M')
-        if now == first_rem or now == second_rem or now == end_rem:
-            bot.send_message(chat_id, "Тестовое напоминание")
-            time.sleep(61)
-        time.sleep(1)
-bot.polling(none_stop=True)
 
+import requests as rq
+import pprint as pr
+
+ht =('https://jsonplaceholder.typicode.com/posts')
+db =  {"title":"foo",
+       "body":"bar",
+       "userId":1
+}
+
+zap = rq.post(ht, data=db)
+pr.pprint(zap.json())
 
--- Test_1.py
+++ Test_1.py
@@ -1,2 +1,16 @@
+import requests
 
+url = 'https://jsonplaceholder.typicode.com/posts'
+
+dataSP = {
+    "tittle" : "Тестовый post запрос",
+    "body" : "Тестовый контент post запроса",
+    "userID" : 2
+}
+
+response = requests.post(url, data = dataSP)
+
+print(response.status_code)
+
+print(f'ответ - {response.json()}')
 
